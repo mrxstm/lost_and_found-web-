078d1369fe62b288a4f3b6e3d435eb272e7c9f66
Satyam Shrestha <52965655+mrxstm@users.noreply.github.com>
2026-02-25 12:41:58 +0545

    feat: dynamic college options for registration form

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,103 +4,74 @@ import { hashPassword } from "../utils/hashPassword.js";
 import { generateToken } from "../utils/jwt-utils.js";
 import bcrypt from "bcrypt";
 
-
-
 export const register = async (req, res) => {
-   
     try {
-    
         const { fullname, username, email, password, phone_no, gender, college } = req.body;
 
-        // invalid or empty fields
+        if (!fullname || fullname.trim() === "") return res.status(400).json({ message: "Fullname is required" });
 
-        if (!fullname || fullname.trim() === "") return res.status(400).json({message:"Fullname is required"});
-
-
-        // username validation
-        if (!username || username.trim() === "") return res.status(400).json({message:"Username is required"});
-
+        if (!username || username.trim() === "") return res.status(400).json({ message: "Username is required" });
         if (!/^[a-zA-Z0-9_]{3,20}$/.test(username)) {
-         return res.status(400).json({ message: "Username must be 3-20 chars, letters, numbers, or underscore" });
+            return res.status(400).json({ message: "Username must be 3-20 chars, letters, numbers, or underscore" });
         }
 
-
-        if (!email || email.trim() === "") return res.status(400).json({message:"Email is required"});
-        
-        // email format validation
+        if (!email || email.trim() === "") return res.status(400).json({ message: "Email is required" });
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) {
-        return res.status(400).json({ message: "Invalid email format" });
+            return res.status(400).json({ message: "Invalid email format" });
         }
 
-        
-        if (!password || password.trim() === "") return res.status(400).json({message:"Password is required"});
-        // if (!c_password || c_password.trim() === "") return res.status(400).json({message:"Confirm password is required"});
-
-        //password length and contains upper lower special character and number validation 
+        if (!password || password.trim() === "") return res.status(400).json({ message: "Password is required" });
         const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
         if (!passwordRegex.test(password)) {
-        return res.status(400).json({ 
-            message: "Password must be 8+ chars, include uppercase, lowercase, number & special char" 
-        });
+            return res.status(400).json({
+                message: "Password must be 8+ chars, include uppercase, lowercase, number & special char"
+            });
         }
 
-
-        // check if password and confirm password fields match
-        // if (password !== c_password) return res.status(400).json({message:"Passwords do not match"});
-
-
-        if (!phone_no || phone_no.trim() === "") return res.status(400).json({message:"Phone number is required"});
-
-        //number validation (should have 10 digits and starts with 98 97)
+        if (!phone_no || phone_no.trim() === "") return res.status(400).json({ message: "Phone number is required" });
         const numberRegex = /^9[87]\d{8}$/;
         if (!numberRegex.test(phone_no)) {
-        return res.status(400).json({ message: "Invalid phone number format" });
+            return res.status(400).json({ message: "Invalid phone number format" });
         }
 
-        if (!gender || gender.trim() === "") return res.status(400).json({message:"Gender is required"});
+        if (!gender || gender.trim() === "") return res.status(400).json({ message: "Gender is required" });
 
-        //College field validation
-        if (!college || college.trim() === "") return res.status(400).json({message:"College is required"});
+        if (!college) return res.status(400).json({ message: "College is required" });
 
-        // email already registered
-
-        const existingUser = await Users.findOne({where: {email}});
+        const existingUser = await Users.findOne({ where: { email } });
         if (existingUser) {
             return res.status(400).json({ message: "Email already registered" });
         }
 
-        //College id validation
-        const selectedCollege = await College.findOne({where: {name : college}});
+        const selectedCollege = await College.findOne({ where: { id: college } });
         if (!selectedCollege) return res.status(400).json({ message: "Invalid College selected" });
 
         const college_id = selectedCollege.id;
-        
-        const existingPhone = await Users.findOne({where: {phone_no}});
-        if(existingPhone) {
+
+        const existingPhone = await Users.findOne({ where: { phone_no } });
+        if (existingPhone) {
             return res.status(400).json({ message: "Phone number already registered" });
         }
 
-        // password hash using bcrypt
-        const password_hash = await hashPassword(password)      
+        const password_hash = await hashPassword(password);
 
         const user = await Users.create({
-            fullname, 
-            username, 
-            email, 
-            password_hash, 
-            phone_no, 
-            gender, 
+            fullname,
+            username,
+            email,
+            password_hash,
+            phone_no,
+            gender,
             college_id
         });
-        res.status(201).json({message: "User registration successful"})
 
+        res.status(201).json({ message: "User registration successful" });
 
-        
-    } catch(e) {
-        res.status(500).json({message:e.message})
+    } catch (e) {
+        res.status(500).json({ message: e.message });
     }
-}
+};
 
 export const login = async(req,res) => {
 
